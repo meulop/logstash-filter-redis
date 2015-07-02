8366f4f4685f3e8c75fd64c520e0f8644a3f61c5
Dave Taylor <david.a.taylor@gmail.com>
2015-07-02 08:07:17 +0000

    Second test, but also some timing issue with first one?

--- spec/filters/redis_spec.rb
+++ spec/filters/redis_spec.rb
@@ -5,10 +5,32 @@ require "pp"
 require "json"
 
 describe LogStash::Filters::Redis do
+  before(:all) do
+    @redis = Redis.new()
+  end
 
   after(:each) do
-    @redis = Redis.new()
-    @redis.del("logstash-filter-redis-test*")
+    sleep(0.2)
+    @redis.keys("logstash-filter-redis-test*").each do |k|
+      @redis.del(k)
+    end
+  end
+
+  describe "Ignores irrelevant key" do
+    config <<-CONFIG
+      filter {
+        redis {
+          store_tag => "BEGIN"
+          retrieve_tag => "END"
+          key => "logstash-filter-redis-test"
+        }
+      }
+    CONFIG
+
+    sample({"message" => "Test message", "tags" => ["APACHE"]}) do
+      insist { @redis.get("logstash-filter-redis-test") } == nil
+    end
+
   end
 
   describe "Stores key and values" do
@@ -23,10 +45,8 @@ describe LogStash::Filters::Redis do
     CONFIG
 
     sample({"message" => "Storing message", "tags" => ["BEGIN"]}) do
-      @redis = Redis.new()
       @stored = @redis.get("logstash-filter-redis-test")
       insist { subject["tags"].include?("STORED") } == true
-      insist { subject["tags"].include?("BUGGERED") } == false
       insist { JSON.parse(@stored)["message"] } == "Storing message"
     end
   end
